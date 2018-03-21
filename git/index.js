36280714c4fa757ff6d742e2a95c9da141510a5b
Keidrych Anton-Oates <keidrych@tayloredtechnology.net>
2018-03-21 11:13:28 +1000

    feat(git): implement redrun for multi-task and lint-commit

--- git/index.js
+++ git/index.js
@@ -5,14 +5,21 @@ module.exports = () => {
 		'@commitlint/cli',
 		'@commitlint/config-conventional',
 		'@commitlint/lint',
-		'@commitlint/prompt-cli'
+		'@commitlint/prompt-cli',
+		'lint-staged'
 	]
 
 	// Package.json
 	packageJson()
 		.setScript('commit', 'commit')
-		.setScript('precommit', 'redrun -p format:*')
+		.setScript('commitmsg', 'commitlint -e $GIT_PARAMS')
+		.setScript('format', 'redrun -p format:*')
 		.set('commitlint', {extends: ['@commitlint/config-conventional']})
+		.set('lint-staged', {
+			'*.{js}': ['xo', 'git add'],
+			'*.{css,md}': ['prettier --write', 'git add']
+		})
+		.set('husky', {hooks: {'pre-commit': 'lint-staged'}})
 		.save()
 
 	// Install
