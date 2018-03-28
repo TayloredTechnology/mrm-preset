67e7d29a4a0085825cc6b63f09f51f5f6588b7a5
Keidrych Anton-Oates <keidrych@tayloredtechnology.net>
2018-03-28 22:20:44 +1000

    fix(labels): purge default GitHub labels

--- labels/index.js
+++ labels/index.js
@@ -13,28 +13,49 @@ const labelConfig = {
 }
 
 module.exports = () => {
-	const labels = json('git-labels.json', {
-		default: [
-			{name: 'Status: Abandoned', color: '#000000'},
-			{name: 'Status: Accepted', color: '#009800'},
-			{name: 'Status: Available', color: '#bfe5bf'},
-			{name: 'Status: Blocked', color: '#e11d21'},
-			{name: 'Status: Completed', color: '#006b75'},
-			{name: 'Status: In Progress', color: '#cccccc'},
-			{name: 'Status: Pending', color: '#fef2c0'},
-			{name: 'Status: Review Needed', color: '#fbca04'},
-			{name: 'Status: Revision Needed', color: '#e11d21'},
-			{name: 'Type: Bug', color: '#e11d21'},
-			{name: 'Type: Maintenance', color: '#fbca04'},
-			{name: 'Type: Enhancement', color: '#84b6eb'},
-			{name: 'Type: Question', color: '#cc317c'},
-			{name: '┻━┻ ︵ ლ(⌒-⌒ლ)', color: '#cc6699'}
-		]
-	})
-	if (!labels.exists()) labels.save()
+	const labelPurge = [
+		{name: 'bug', color: ''},
+		{name: 'duplicate', color: ''},
+		{name: 'enhancement', color: ''},
+		{name: 'good first issue', color: ''},
+		{name: 'invalid', color: ''},
+		{name: 'question', color: ''},
+		{name: 'wontfix', color: ''}
+	]
 
 	gitLabel
-		.add(labelConfig, labels.get().default)
+		.remove(labelConfig, labelPurge)
+		.then(console.log)
+		.catch(console.log)
+
+	const labelData = [
+		{name: 'Status: Abandoned', color: '#000000'},
+		{name: 'Status: Accepted', color: '#009800'},
+		{name: 'Status: Available', color: '#bfe5bf'},
+		{name: 'Status: Blocked', color: '#e11d21'},
+		{name: 'Status: Completed', color: '#006b75'},
+		{name: 'Status: In Progress', color: '#cccccc'},
+		{name: 'Status: Pending', color: '#fef2c0'},
+		{name: 'Status: Review Needed', color: '#fbca04'},
+		{name: 'Status: Revision Needed', color: '#e11d21'},
+		{name: 'Type: Bug', color: '#e11d21'},
+		{name: 'Type: Duplicate', color: '#ffffff'},
+		{name: 'Type: Enhancement', color: '#84b6eb'},
+		{name: 'Type: Maintenance', color: '#fbca04'},
+		{name: 'Type: Question', color: '#cc317c'},
+		{name: 'ʕノ•ᴥ•ʔノ ︵ ┻━┻', color: '#003366'}
+	]
+	const labels = json('git-labels.json', {labelData})
+
+	labels.merge(labelData).save()
+
+	let gitLabelData = Object.keys(labels.get()).map(k => {
+		if (k !== 'default') return labels.get()[k]
+	})
+
+	gitLabelData.pop()
+	gitLabel
+		.add(labelConfig, gitLabelData)
 		.then(console.log)
 		.catch(console.log)
 }
