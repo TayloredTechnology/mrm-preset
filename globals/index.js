9bd195fcd5b4611a772bbfe3f7d827c972ac9785
Keidrych Anton-Oates <keidrych@tayloredtechnology.net>
2018-03-29 11:10:46 +1000

    feat(globals): global npm modules necessary for helping

---
+++ globals/index.js
@@ -0,0 +1,36 @@
+const installGlobal = require('npm-install-global')
+const isGlobal = require('get-installed-path')
+
+module.exports = () => {
+	const globalInstalls = [
+		'snyk',
+		'redrun',
+		'@tayloredtechnology/oneflow',
+		'finepack',
+		'authenticator',
+		'nyc'
+	]
+
+	const toInstall = []
+	globalInstalls.map(install => {
+		try {
+			isGlobal.getInstalledPathSync(install, {
+				local: false
+			})
+		} catch (err) {
+			toInstall.push(install)
+		}
+		return true
+	})
+
+	if (toInstall.length > 0)
+		installGlobal.install(toInstall, err => {
+			if (err) throw err
+		})
+
+	// Additional manual steps to run
+	if (toInstall.includes('snyk'))
+		console.log('Sign into Snyk via `synk auth` to finish installing')
+}
+
+module.exports.description = 'Ensure Global toolset is installed'
